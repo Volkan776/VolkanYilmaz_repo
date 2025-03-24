748b0979d30749a83c6c0bdedccc748370a1f262
Volkan776 <volkan.yilmaz1@std.yeditepe.edu.tr>
2025-03-24 17:18:09 +0300

    2 basamak yazılı

---
+++ main.py
@@ -0,0 +1,16 @@
+birler = ["", "Bir", "İki", "Üç", "Dört", "Beş", "Altı", "Yedi",
+"Sekiz", "Dokuz"]
+onlar = ["", "On", "Yirmi", "Otuz", "Kırk", "Elli", "Altmış"
+, "Yetmiş", "Seksen", "Doksan"]
+
+
+def okunus(sayi):
+    birinci = sayi % 10
+    ikinci = sayi // 10
+
+    return onlar[ikinci] + " " + birler[birinci]
+
+
+sayi = int(input("Sayı:"))
+
+print(okunus(sayi))
