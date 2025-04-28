017183287a6d2396a2d607a107c8893a6126b9e7
Volkan776 <volkan.yilmaz1@std.yeditepe.edu.tr>
2025-04-28 17:51:41 +0300

    3.ödev 5.soru.py

---
+++ 3.ödev 5.soru.py
@@ -0,0 +1,6 @@
+my_list = [5, 2, -5, 10, 23, -21,54, 1, -80]
+biggest_int = my_list[0]
+for num in my_list:
+    if num > biggest_int:
+        biggest_int = num
+print("En büyük sayı:", biggest_int)
